2cf07b401bb5b010664ef19659dc9d53ac14f34f
bernas1305 <bernardo.santos13@gmail.com>
2023-05-15 15:01:07 +0100

    feat: esqueleto reboque

--- js/main-script.js
+++ js/main-script.js
@@ -12,6 +12,10 @@ function createScene(){
     scene = new THREE.Scene();
     scene.background = new THREE.Color(0xcbd0b9);
 
+    scene.add(new THREE.AxisHelper(10));
+
+    createTrailer(0, 0, 0);
+
 }
 
 //////////////////////
@@ -41,7 +45,82 @@ function createCamera() {
 /* CREATE OBJECT3D(S) */
 ////////////////////////
 
+/* -------------------------------------------------- */
+/*                      TRAILER                       */
+/* -------------------------------------------------- */
 
+
+function addContainer(obj, x, y, z){
+    'use strict';
+
+    geometry = new THREE.CubeGeometry(8, 8, 19);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y, z);
+    obj.add(mesh);
+}
+
+function addLeftBackWheel(obj, x, y, z){
+    'use strict';
+
+    new THREE.TorusGeometry(2, 1.5, 16, 100);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y, z);
+    obj.add(mesh);
+}
+
+function addRightBackWheel(obj, x, y, z){
+    'use strict';
+
+    new THREE.TorusGeometry(2, 1.5, 16, 100);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y, z);
+    obj.add(mesh);
+}
+
+function addLeftFrontWheel(obj, x, y, z){
+    'use strict';
+
+    new THREE.TorusGeometry(2, 1.5, 16, 100);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y, z);
+    obj.add(mesh);
+}
+
+function addRightFrontWheel(obj, x, y, z){
+    'use strict';
+
+    new THREE.TorusGeometry(1, 0.75, 16, 100);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y, z);
+    obj.add(mesh);
+}
+
+function addConnector(obj, x, y, z){
+    'use strict';
+
+    geometry = new THREE.CubeGeometry(1, 1, 1);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y, z);
+    obj.add(mesh);
+}
+
+function createTrailer(x, y, z){
+    'use strict';
+
+    var trailer = new THREE.Object3D();
+    addContainer(trailer, 0, 0, 0);
+    addLeftBackWheel(trailer, -3.25, -5, 8.5);
+    addRightBackWheel(trailer, 3.25, -5, 8.5);
+    addLeftFrontWheel(trailer, -3.25, -5, 6);
+    addRightFrontWheel(trailer, 3.25, -5, 6);
+    addConnector(trailer, 0, 0, 0);
+
+    scene.add(trailer);
+
+    trailer.position.x = x;
+    trailer.position.y = y;
+    trailer.position.z = z;
+}
 
 
 //////////////////////
